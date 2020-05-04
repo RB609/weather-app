0b33f436145d7a7533292180e41ce14239305233
rb609 <rbhola89@gmail.com>
2020-05-05 01:24:34 +0530

    remove unnecessary clog calls

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,3 @@
-
-
-console.log("Client side js loaded!");
-
-// fetch('http://puzzle.mead.io/puzzle').then((response) => {
-//     response.json().then((data) => {
-//         console.log(data);
-//     })
-// })
-
-
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const msgOne = document.querySelector('#p1');
